08d56c4854e3e5133b4931016a0c6eda30787663
maruyu998 <maruyu998@gmail.com>
2025-06-30 11:35:12 +0900

    refactor: remove deprecated configuration files for default, development, and production environments

--- config/default.ts
+++
@@ -1,5 +0,0 @@
-import { DAY, MINUTE } from 'maruyu-webcommons/commons/utils/time';
-
-export const sessionKeepDuration = 30 * DAY;
-export const userInfoKeepDuration = MINUTE;
-export const authSessionKeepDuration = 5 * MINUTE;
--- config/development.ts
+++
@@ -1,6 +0,0 @@
-import * as secret from './secret';
-
-export const mongoPath = "mongodb://host.docker.internal:27017/calendar";
-export const sessionSecret = "gP75gbcgWgj.5%qSuE2rYv83S6-R~8$R#4HCJ_,C";
-export const hashSalt = "7Vh2.imC58KK6bq,/XL)Hk~B,u9euJLt&yCPBfnL";
-export const providerUserId = secret.providerUserId;
